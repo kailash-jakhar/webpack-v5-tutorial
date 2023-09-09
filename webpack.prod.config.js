cdc4348bc2cc536808cd0f2ab3ab642b086f9933
Kailash <kailash@Kailashs-MacBook-Pro.local>
2023-09-09 22:49:13 +0530

    app

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -12,7 +12,7 @@ module.exports = {
     output : {
         filename : '[name].[contenthash].js',
         path : path.resolve(__dirname,'./dist'),
-        publicPath:'/static/'
+       
     },
     mode : 'production',
     optimization : {
